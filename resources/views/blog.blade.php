c2fb25cbf2dd7d6666a21ff48cbc635a8fcf682c
aalaa elsayed <146981656+aalaaelsayed@users.noreply.github.com>
2023-12-01 15:34:03 +0200

    laravel
    my website

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>new website</title>
+</head>
+<body>
+    <h1>blog</h1>
+    <h2>science</h2>
+    <h2>math</h2>
+    <h2>sports</h2>
+    <h2>medical</h2>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//Route::get('/', function () {
+ //   return view('welcome');
+//});
+/////////////////////////////
+Route::fallback(function(){
+    return redirect('/');
+});
+Route::get('about',function(){
+
+    return view('about');
+});
+//////////////////
+Route::get('contact_us',function(){
+
+    return view('contact-us');
+});
+
+//////////////////////
+
+Route::prefix('blog')->group(function(){
+    //blog home
+    Route::get('/',function(){
+
+    return view('blog');
+});
+///////////////////////
+
+Route::get('science',function(){
+
+    return'science for blog';
+});
+/////////////////////
+Route::get('math',function(){
+
+    return'math for blog';
+});
+///////////////////////
+Route::get('sports',function(){
+
+    return'sports for blog';
+});
+//////////////////////
+Route::get('medical',function(){
+
+    return'medical for blog';
+});
+});
